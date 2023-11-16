226e2f0a75c9ccbbd60fe4aa0ab0dc77f8973a2e
bendinuralif <bendipenthol@gmail.com>
2023-11-17 02:03:18 +0700

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
-import logoWhite from '../assets/images/'
+import logoWhite from '../assets/images/logo-white.png'
 
 const Footer = () => {
   return (
@@ -10,44 +10,66 @@ const Footer = () => {
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="" class="h-8 me-3" alt="FlowBite Logo" />
+              <a href="#" class="flex items-center">
                   <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Lensa Karya</span>
-                  
+                  <img src= {logoWhite} class="h-8 me-3" alt="FlowBite Logo" />
               </a>
               <p className='text-white w-[269px]'>Membantu dalam menemukan jasa dalam mengabadikan momen anda yang indah.</p>
           </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-4">
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Navigasi</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
+                          <a href="/" class="hover:underline">Beranda</a>
                       </li>
-                      <li>
-                          <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
+                      <li class="mb-4">
+                          <a href="aboutus" class="hover:underline">Tentang Kami</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="layanan" class="hover:underline">Layanan</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="portofolio" class="hover:underline">Portofolio</a>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Tentang Kami</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Github</a>
+                          <a href="#" class="hover:underline ">Tentang</a>
                       </li>
                       <li>
-                          <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Discord</a>
+                          <a href="#" class="hover:underline">Gabung</a>
                       </li>
                   </ul>
               </div>
               <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Layanan</h2>
                   <ul class="text-gray-500 dark:text-gray-400 font-medium">
                       <li class="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
+                          <a href="#" class="hover:underline">Fotografer</a>
                       </li>
-                      <li>
-                          <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">Videografer</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="portofolio" class="hover:underline">Portofolio</a>
+                      </li>
+                  </ul>
+              </div>
+              <div>
+                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Portofolio</h2>
+                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">Kategori</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">Foto</a>
+                      </li>
+                      <li class="mb-4">
+                          <a href="#" class="hover:underline">Video</a>
                       </li>
                   </ul>
               </div>
@@ -55,7 +77,7 @@ const Footer = () => {
       </div>
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Lensa Karya</a>. All Rights Reserved.
           </span>
           <div class="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Layout>
       <a href='https://www.youtube.com' className='flex-none rounded-lg w-64 h-10 bg-teal-500 font-bold text-xl flex justify-center items-center'>
-        Button
+        Home
       </a>
     </Layout>
   );
