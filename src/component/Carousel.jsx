3e7b7a66773255d4c010404e0c230124cde24a82
bendinuralif <bendipenthol@gmail.com>
2023-11-21 01:04:16 +0700

    update about

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -6,15 +6,15 @@ const Carousel = () => {
   return (
     <>
       <div>
-        <div id='controls-carousel' className='relative w-full' data-carousel='static'>
+        <div id='controls-carousel' className='relative w-full mt-11' data-carousel='static'>
           {/* <!-- Carousel wrapper --> */}
           <div className='relative h-56 overflow-hidden rounded-lg md:h-96'>
             {/* <!-- Item 1 --> */}
-            <div className='hidden duration-700 ease-in-out' data-carousel-item>
+            <div className='none duration-700 ease-in-out' data-carousel-item>
               <img src={Images} className='absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' alt='...' />
             </div>
             {/* <!-- Item 2 --> */}
-            <div className='hidden duration-700 ease-in-out' data-carousel-item='active'>
+            <div className='none duration-700 ease-in-out' data-carousel-item='active'>
               <img src={Images2} className='absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' alt='...' />
             </div>
           </div>
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import Layout from './Layout';
-import '../assets/css/aboutus.css';
+import Images from '../assets/images/andre.png'
 
 const Aboutus = () => {
   return (
     <Layout>
-      <div className='aboutContent'>ABOUT</div>
-      <p className='additionalText'>Lensa Karya menyediakan fotografer dan videografer profesional untuk mengabadikan momen berharga dalam hidup Anda</p>
-      <div className='featureTitle'>MENGAPA HARUS</div>
-      <div className='companyName'>LENSA KARYA</div>
-      <p className='descriptionText'>Lensa Karya, ahli cerita visual, menangkap ratusan momen istimewa dengan kreativitas dan keahlian. Kami berbagi keindahan melalui gambar dan film unik Anda.</p>
+      <div>
+        <div className='text-center font-inter text-4xl font-bold pt-10 mt-[256px] text-[#EC0000]'>
+          ABOUT
+        </div>
+        <p className='text-center font-inter text-2xl font-normal p-4 mr-11 ml-11'>
+          Lensa Karya menyediakan fotografer dan videografer profesional untuk mengabadikan momen berharga dalam hidup Anda
+        </p>
+      </div>
+      <div className='flex m-[188px] mt-[600px]'>
+        <div>
+          <div className='font-inter text-4xl font-bold pt-10 pl-4'>
+            MENGAPA HARUS
+          </div>
+          <div className='font-inter text-4xl font-bold pl-4 text-[#EC0000]'>
+            LENSA KARYA ?
+          </div>
+          <p className='font-inter text-xl font-normal p-4'>
+            Lensa Karya, ahli cerita visual, menangkap ratusan momen istimewa dengan kreativitas dan keahlian. Kami berbagi keindahan melalui gambar dan film unik Anda.
+          </p>
+        </div>
+        <div className='rounded-lg bg-white p-4 shadow-md mr-[]'>
+          <img src={Images} alt=""  />
+        </div>
+      </div>
+      <div className='flex '>
+        <div className='rounded-lg bg-white p-4 shadow-md ml-[190px]'>
+          <img src={Images} alt="" />
+        </div>
+        <div className='ml-11 mr-11'>
+        <div className='font-inter text-4xl font-bold pl-4 '>
+          GABUNG BERSAMA 
+          <div className='text-[#EC0000]'>
+            KAMI
+          </div>
+        </div>
+          <p className='font-inter text-xl font-normal p-4'>
+          "Bergabunglah dengan Lensa Karya, sampaikan kisah unik Anda melalui lensa kami. Dukungan pertumbuhan dan ciptakan seni inspiratif bersama."
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
