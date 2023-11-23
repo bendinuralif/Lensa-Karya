cdf82b2b2d36f78e0fbb3fe539fa4ca9f8868c51
Bendi Nur Alif <113667806+bendinuralif@users.noreply.github.com>
2023-11-23 08:11:30 +0700

    Update Carousel.jsx

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -5,13 +5,13 @@ import Images2 from '../assets/images/carousel2.png';
 const CarouselItem = () => {
   return (
     <>
-      <div className='h-56 sm:h-64 xl:h-80 2xl:h-96'>
-        <Carousel>
-          <img src={Images} alt='...' />
+      <div className='h-56 sm:h-64 xl:h-80 2xl:h-96 mb-8'>
+        <Carousel style={{ width: '100%', height: '160%' }}>
+          <img src={Images} alt='...' className='object-cover w-full h-full' />
+          <img src={Images2} alt='...' className='object-cover w-full h-full' />
+          {/* <img src={Images} alt='...' />
           <img src={Images2} alt='...' />
-          <img src={Images} alt='...' />
-          <img src={Images2} alt='...' />
-          <img src={Images} alt='...' />
+          <img src={Images} alt='...' /> */}
         </Carousel>
       </div>
     </>
