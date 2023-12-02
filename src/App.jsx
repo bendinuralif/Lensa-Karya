f0ffb6724ce5814c684b74a0d98745ecc1e4edb9
Bendi Nur Alif <113667806+bendinuralif@users.noreply.github.com>
2023-12-02 09:26:13 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import Daftar from './pages/Daftar';
 import Navbarlogin from './component/Navbarlogin';
 import Layoutlogin from './pages/Layoutlogin';
 import Profile from './pages/Profile';
+import Portowedd from './pages/Portowedd';
+
 
 function App() {
   return (
@@ -31,9 +33,4 @@ function App() {
         <Route path='/navbarlogin' element={<Navbarlogin />} />
         <Route path='/layoutlogin' element={<Layoutlogin />} />
         <Route path='/profile' element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+        <Route path='/portowedd' element={<Portowedd/>}/>
