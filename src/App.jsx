d4b9c83540e411a177e32a69b81a7245059b1e25
Bendi Nur Alif <113667806+bendinuralif@users.noreply.github.com>
2023-12-02 09:24:12 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import Aboutus from './pages/Aboutus';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layanan from './pages/Layanan';
 import Portfolio from './pages/Portofolio';
-import Portowedd from './pages/Portowedd';
+import Login from './pages/Login';
+import Lupasandi from './pages/Lupasandi';
+import Verifikasi from './pages/Verifikasi';
+import Katasandibaru from './pages/Katasandibaru';
+import Daftar from './pages/Daftar';
+import Navbarlogin from './component/Navbarlogin';
+import Layoutlogin from './pages/Layoutlogin';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -16,7 +23,14 @@ function App() {
         <Route path='/aboutus' element={<Aboutus />} />
         <Route path='/layanan' element={<Layanan />} />
         <Route path='/portofolio' element={<Portfolio />} />
-        <Route path='/portowedd' element={<Portowedd/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/lupasandi' element={<Lupasandi />} />
+        <Route path='/verifikasi' element={<Verifikasi />} />
+        <Route path='/katasandibaru' element={<Katasandibaru />} />
+        <Route path='/daftar' element={<Daftar />} />
+        <Route path='/navbarlogin' element={<Navbarlogin />} />
+        <Route path='/layoutlogin' element={<Layoutlogin />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
