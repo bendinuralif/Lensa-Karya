981a4caab6806537e8c325ecf260d332647acd3f
Ananda Aqilla Aqsa <132327112+nandzzurfavboy@users.noreply.github.com>
2023-12-04 20:40:57 +0700

    update home nanda

--- src/component/Carousel.jsx
+++ src/component/Carousel.jsx
@@ -16,12 +16,13 @@ const CarouselItem = () => {
           Abadikan Moment Berhargamu, <br />
           Untuk Kenangan Indah Kan di Ingat.
         </p>
-        <button
+        <Link
+          to='/layanan'
           type='button'
-          class='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
+          className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'
         >
           Jelajahi Sekarang!
-        </button>
+        </Link>
       </div>
       {/* <div className='absolute top-0 left-0 h-full w-full'>
         <h2 className='text-4xl font-bold'>Lensa Karya</h2>
